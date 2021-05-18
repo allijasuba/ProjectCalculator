fdedd62ab40532798521cbccca4b0b65f0153188
allijasuba <allijasuba@gmail.com>
2021-05-19 02:29:54 +0800

    Added button 2

--- ProjectCalculator/Form1.Designer.cs
+++ ProjectCalculator/Form1.Designer.cs
@@ -30,22 +30,33 @@ namespace ProjectCalculator
         private void InitializeComponent()
         {
             this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOne
             // 
-            this.btnOne.Location = new System.Drawing.Point(27, 131);
+            this.btnOne.Location = new System.Drawing.Point(30, 130);
             this.btnOne.Name = "btnOne";
             this.btnOne.Size = new System.Drawing.Size(100, 80);
             this.btnOne.TabIndex = 0;
             this.btnOne.Text = "1";
             this.btnOne.UseVisualStyleBackColor = true;
             // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(150, 130);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(100, 80);
+            this.btnTwo.TabIndex = 1;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.btnTwo);
             this.Controls.Add(this.btnOne);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@ namespace ProjectCalculator
         #endregion
 
         private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
     }
 }
 
